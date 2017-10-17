6400ff8027f8aaaf94b062cedf67aaa0e9c7a282
Giuseppe Zerbo <giuseppe.zerbo@gmail.com>
2017-10-17 17:38:01 +0200

    Fixed wrong Red Lobster dependency for Lumo scripts.

--- lumo-test.sh
+++ lumo-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-lumo -c ../libs/redlobster-0.2.2-SNAPSHOT.jar:src/cljs:src/clj:test/cljs -K -m shrimp.tests
+lumo -D org.clojars.pepzer/redlobster:0.2.2 -c src/cljs:src/clj:test/cljs -K -m shrimp.tests
 
